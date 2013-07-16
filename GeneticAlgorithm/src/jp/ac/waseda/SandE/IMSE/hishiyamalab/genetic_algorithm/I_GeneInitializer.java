47fe4829fff5f184fea1e65fce30dafd9de086a9
Itaru Suzuki <itaru.suzuki@datasection.co.jp>
2013-07-16 11:52:31 +0900

    プロジェクトの作成．基本的なインタフェースを記述し，実装は後回しに．

---
+++ GeneticAlgorithm/src/jp/ac/waseda/SandE/IMSE/hishiyamalab/genetic_algorithm/I_GeneInitializer.java
@@ -0,0 +1,7 @@
+package jp.ac.waseda.SandE.IMSE.hishiyamalab.genetic_algorithm;
+
+import java.util.List;
+
+public interface I_GeneInitializer {
+	List<I_Gene> makeInitGenes();
+}
---
+++ GeneticAlgorithm/src/jp/ac/waseda/SandE/IMSE/hishiyamalab/genetic_algorithm/I_Mutator.java
@@ -0,0 +1,5 @@
+package jp.ac.waseda.SandE.IMSE.hishiyamalab.genetic_algorithm;
+
+public interface I_Mutator {
+	I_Gene mutate(I_Gene gene);
+}
